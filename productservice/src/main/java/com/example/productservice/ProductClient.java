e418fbc8b661c3ee328c033f74de9ab419adf5c4
Meet-2103 <parekhmeet.28@gmail.com>
2026-06-02 19:09:40 +0530

    Service discovery completed

--- productservice/src/main/java/com/example/productservice/ProductClient.java
+++ productservice/src/main/java/com/example/productservice/ProductClient.java
@@ -1,12 +1,13 @@
 package com.example.productservice;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "${feign.client.product-service.url}")
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
     @GetMapping("products/{id}")
     String getProductById(@PathVariable("id") String id);
 }
+
+
--- productservice/src/main/java/com/example/productservice/ProductController.java
+++ productservice/src/main/java/com/example/productservice/ProductController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
-
 public class ProductController {
 
     @GetMapping("{id}")
